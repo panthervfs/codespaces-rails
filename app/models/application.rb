16b0ce0e06bd12ade88e37f4422496987d24e95e
Joseph Felix <98041255+panthervfs@users.noreply.github.com>
2024-03-22 04:19:01 +0000

    add more state logic

--- app/models/application.rb
+++ app/models/application.rb
@@ -26,30 +26,19 @@ class Application < ApplicationRecord # rubocop:disable Style/Documentation
     end
 
     event :update_status do
-      transitions to: :active, guard: proc { repo_status == ACTIVE && config.present? }
-      transitions to: :archived, guard: proc { repo_status == ARCHIVED }
-      transitions to: :merge_queue_disabled, guard: proc { repo_status == MERGE_QUEUE_DISABLED }
-      transitions to: :repo_not_accessible, guard: proc { repo_status == REPO_NOT_ACCESSIBLE }
-      transitions to: :repo_not_found, guard: proc { repo_status == REPO_NOT_FOUND }
-      transitions to: :config_not_found, guard: proc { config.nil? && config_error.is_a?(ConfigNotFound) }
-      transitions to: :invalid_config_error, guard: proc { config.nil? && config_error.is_a?(InvalidConfigError) }
-      transitions to: :inactive, guard: proc { config.nil? && config_error.is_a?(AppNotAllowed) }
+      transitions to: :active, guard: proc { |repo_state_resp| repo_status(repo_state_resp) == ACTIVE && deployment_config.present? }
+      transitions to: :archived, guard: proc { |repo_state_resp| repo_status(repo_state_resp) == ARCHIVED }
+      transitions to: :merge_queue_disabled, guard: proc { |repo_state_resp| repo_status(repo_state_resp) == MERGE_QUEUE_DISABLED }
+      transitions to: :repo_not_found, guard: proc { |repo_state_resp| repo_status(repo_state_resp) == REPO_NOT_FOUND }
+      transitions to: :repo_not_accessible, guard: proc { deployment_config.nil? && config_error.is_a?(RepoNotAccessible) }
+      transitions to: :config_not_found, guard: proc { deployment_config.nil? && config_error.is_a?(ConfigNotFound) }
+      transitions to: :invalid_config_error, guard: proc { deployment_config.nil? && config_error.is_a?(InvalidConfigError) }
+      transitions to: :inactive, guard: proc { deployment_config.nil? && config_error.is_a?(AppNotAllowed) }
     end
 
     event :activate do
       transitions to: :active, guard: proc { true }
     end
-  end
-
-  def log_status_change
-    return unless persisted?
-    return if status == aasm.to_state
-
-    puts "changing from #{status} to #{aasm.to_state} (event: #{aasm.current_event})"
-  end
-
-  def notify_inactive
-    puts "notifying that #{name} is inactive..."
   end
 
   ACTIVE = 0
@@ -72,6 +61,17 @@ class Application < ApplicationRecord # rubocop:disable Style/Documentation
     repo_not_found: 7
   }
 
+  def log_status_change
+    return unless persisted?
+    return if status == aasm.to_state
+
+    puts "changing from #{status} to #{aasm.to_state} (event: #{aasm.current_event})"
+  end
+
+  def notify_inactive
+    puts "notifying that #{name} is inactive..."
+  end
+
   class AppNotAllowed < StandardError # rubocop:disable Style/Documentation
     def initialize(msg = 'Application not allowed')
       super
@@ -90,7 +90,15 @@ class Application < ApplicationRecord # rubocop:disable Style/Documentation
     end
   end
 
-  def config
+  class RepoNotAccessible < StandardError # rubocop:disable Style/Documentation
+    def initialize(msg = 'Repository not accessible')
+      super
+    end
+  end
+
+  def deployment_config
+    return nil if @config_error
+
     puts 'STAGE: retrieving config...'
     @config ||= populate_config
   rescue StandardError => e
@@ -122,33 +130,37 @@ class Application < ApplicationRecord # rubocop:disable Style/Documentation
     puts 'STAGE: returning config...'
     # { 'deployment' => { 'strategy' => 'kubernetes', 'prerequisites' => current_second.odd? } }
     { 'deployment' => { 'strategy' => 'kubernetes', 'prerequisites' => true } }
+    raise InvalidConfigError
   end
 
-  def repo_status
-    @repo_status ||= populate_repo_status
+  def repo_status(repo_status_resp = nil)
+    @repo_status ||= populate_repo_status(repo_status_resp)
   end
 
-  def populate_repo_status
+  def populate_repo_status(repo_status_resp = nil) # rubocop:disable Metrics/MethodLength,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
     puts 'STAGE: populating repo status...'
-    # current_second = Time.now.sec
+    repo_state = nil
+    unless repo_status_resp.nil?
+      puts 'STAGE: repo status response provided...'
+      puts "STAGE: repo_status #{repo_status_resp}"
+      if !repo_status_resp[:repo_found]
+        repo_state = REPO_NOT_FOUND
+      elsif repo_status_resp[:is_archived]
+        repo_state = ARCHIVED
+      elsif pipelines_enabled && !repo_status_resp[:merge_queue_id].nil?
+        repo_state = ACTIVE
+      elsif pipelines_enabled && repo_status_resp[:merge_queue_id].nil?
+        repo_state = MERGE_QUEUE_DISABLED
+      elsif !pipelines_enabled
+        repo_state = ACTIVE
+      end
+    end
+    puts "STAGE: returning repo state #{repo_state}..."
+    repo_state
+  end
 
-    # case current_second % 10
-    # when 3
-    #   puts 'STAGE: repo archived...'
-    #   return ARCHIVED
-    # when 4
-    #   puts 'STAGE: repo merge queue disabled...'
-    #   return MERGE_QUEUE_DISABLED
-    # when 5
-    #   puts 'STAGE: repo not accessible...'
-    #   return REPO_NOT_ACCESSIBLE
-    # when 6
-    #   puts 'STAGE: repo not found...'
-    #   return REPO_NOT_FOUND
-    # end
-
-    puts 'STAGE: repo active...'
-    ACTIVE
+  def pipelines_enabled
+    true
   end
 
   def create_deployment
